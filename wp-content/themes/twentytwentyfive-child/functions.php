6c98b711ea77493ef743aab664ee585b9644a19c
faariha1tippu <faariha1tippu@gmail.com>
2026-04-14 00:55:53 +0000

    Final Exam

---
+++ wp-content/themes/twentytwentyfive-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+function twentytwentyfive_child_enqueue_styles() {
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array('parent-style'),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'twentytwentyfive_child_enqueue_styles');
